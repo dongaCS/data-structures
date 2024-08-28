d48276f50fee876139a52769f6757945b3592d3b
dongaCS <109496734+dongaCS@users.noreply.github.com>
2024-08-28 15:14:28 -0400

    implemented: contains, size, isEmpty, reverse, removeFirst and removeLast

--- LinkedList/src/SinglyLinkedList.java
+++ LinkedList/src/SinglyLinkedList.java
@@ -12,8 +12,11 @@ public class SinglyLinkedList<E> {
     }
 
     private Node<E> head;
+    private int size;
+
     SinglyLinkedList() {
         this.head = null;
+        this.size = 0;
     }
 
 
@@ -22,6 +25,7 @@ public class SinglyLinkedList<E> {
      *****************/
     // add element to end of list
     void append(E data) {
+        size++;
         if (head == null) { // if head is null, then it's a new list
             head = new Node<>(data);
             return;
@@ -41,6 +45,7 @@ public class SinglyLinkedList<E> {
         }
         if (head.data.equals(data)) { // check if head is data we're looking for
             head = head.next;
+            size--;
             return;
         }
         // traverse through list looking for data
@@ -49,6 +54,7 @@ public class SinglyLinkedList<E> {
             curr = curr.next;
         }
         if (curr.next != null) { // if we find the data, remove reference to it
+            size--;
             curr.next = curr.next.next;
         }
         // else data was never in list to begin with
@@ -71,6 +77,82 @@ public class SinglyLinkedList<E> {
         Node<E> newNode = new Node<>(data);
         newNode.next = head;
         head = newNode;
+        size++;
     }
-}
+
+    // check if element exist in the list
+    boolean contains(E data) {
+        if (head == null) {
+            return false;
+        }
+        if (head.data.equals(data)) {
+            return true;
+        }
+        Node<E> curr = head;
+        while (curr.next != null && !curr.next.data.equals(data)) {
+            curr = curr.next;
+        }
+        return curr.next != null;
+    }
+
+    // returns the size of the list
+    int size() {
+        return size;
+    }
+
+    // returns if list is empty
+    boolean isEmpty() {
+        return head == null;
+    }
+
+    // returns a new list in reverse order
+    SinglyLinkedList<E> reverse() {
+        SinglyLinkedList<E> list = new SinglyLinkedList<E>();
+        if (head == null) {
+            return list;
+        }
+        reverseHelper(list, head);
+        list.size = size;
+        return list;
+    }
+    void reverseHelper(SinglyLinkedList<E> list, Node<E> node) {
+        if (node.next == null) {
+            list.append(node.data);
+        } else {
+            reverseHelper(list, node.next);
+            list.append(node.data);
+        }
+    }
+
+    // removes the first element from the list
+    void removeFirst() {
+        if (head != null) {
+            head = head.next;
+            size--;
+        }
+    }
+
+    // removes the last element from the list
+    void removeLast() {
+        if (head != null) {
+            if (head.next == null) {
+                head = null;
+            } else {
+                Node<E> prev = head;
+                Node<E> curr = head.next;
+                while (curr.next != null) {
+                    prev = curr;
+                    curr = curr.next;
+                }
+                prev.next = null;
+            }
+            size--;
+        }
+    }
+
+
+
+
+
+} // end of class
 
